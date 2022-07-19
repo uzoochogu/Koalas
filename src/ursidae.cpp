3d5b96629672c4ab072adb463baf10960f6cc38d
Uzochukwu Chibueze Ochogu <uzoochogu@yahoo.com>
2022-07-19 19:09:39 +0100

    🛠️Implemented DataTable ctor using DataColumns

--- src/ursidae.cpp
+++ src/ursidae.cpp
@@ -2,7 +2,15 @@
 #include "ursidae.hpp"
 
 
-
-void foo() {
-    std::cout << "I dont know what foo is ! \n";
+template <typename DataType>
+Ursidae::DataTable<DataType>::DataTable(std::vector<DataColumn<DataType>> &input)  
+{
+    /**
+     * @brief Creates a DataTable from a vector of DataColumns
+     * 
+     */
+    for(auto& dataColumn : input) 
+    {
+        column.push_back(dataColumn.col_name);
+    }
 }
